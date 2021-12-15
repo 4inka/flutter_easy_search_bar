54a103cecfaa1f32a18e93c11dee501410d86b4c
Eunice Vassoa <eunicevassoa@gmail.com>
2021-12-15 15:33:45 +0200

    updating example

--- example/lib/main.dart
+++ example/lib/main.dart
@@ -2,10 +2,12 @@ import 'package:easy_search_bar/easy_search_bar.dart';
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(MyHomePage());
+  runApp(const MyHomePage());
 }
 
 class MyHomePage extends StatefulWidget {
+  const MyHomePage({Key? key}) : super(key: key);
+
   @override
   State<MyHomePage> createState() => _MyHomePageState();
 }
@@ -18,13 +20,41 @@ class _MyHomePageState extends State<MyHomePage> {
     return  MaterialApp(
       title: 'Example',
       theme: ThemeData(
-        primarySwatch: Colors.blue
+        primarySwatch: Colors.orange
       ),
       home: Scaffold(
-        appBar: AppBar(
-          title: EasySearchBar(
-            title: 'Example',
-            onSearch: (value) => setState(() => searchValue = value)
+        appBar: EasySearchBar(
+          title: const Text('Example'),
+          isFloating: false,
+          onSearch: (value) => setState(() => searchValue = value),
+          actions: [
+            IconButton(
+              icon: const Icon(
+                Icons.person,
+              ),
+              onPressed: () {},
+            )
+          ]
+        ),
+        drawer: Drawer(
+          child: ListView(
+            padding: EdgeInsets.zero,
+            children: [
+              const DrawerHeader(
+                decoration: BoxDecoration(
+                  color: Colors.blue,
+                ),
+                child: Text('Drawer Header'),
+              ),
+              ListTile(
+                title: const Text('Item 1'),
+                onTap: () => Navigator.pop(context)
+              ),
+              ListTile(
+                title: const Text('Item 2'),
+                onTap: () => Navigator.pop(context)
+              )
+            ]
           )
         ),
         body: Center(
