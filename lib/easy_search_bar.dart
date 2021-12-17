271fa1aaa0f2583f6e244d76432c2f87b22bdd06
Eunice Vassoa <eunicevassoa@gmail.com>
2021-12-17 11:00:58 +0200

    setting async suggestions

--- lib/easy_search_bar.dart
+++ lib/easy_search_bar.dart
@@ -29,12 +29,18 @@ library easy_search_bar;
 
 import 'dart:async';
 
+import 'package:easy_search_bar/widgets/filterable_list.dart';
 import 'package:flutter/material.dart';
 
 class EasySearchBar extends StatefulWidget implements PreferredSizeWidget {
+  final Color? backgroundColor;
   final Color? foregroundColor;
+  final double? elevation;
   final double toolbarHeight;
-  final FutureOr<List<String>>? suggestions;
+  final double suggestionsElevation;
+  final List<String>? suggestions;
+  final Future<List<String>> Function(String value)? asyncSuggestions;
+  final IconThemeData? iconTheme;
   /// The title to be displayed inside appBar
   final Text title;
   final List<Widget> actions;
@@ -47,19 +53,42 @@ class EasySearchBar extends StatefulWidget implements PreferredSizeWidget {
   final bool isFloating;
   /// Sets custom cursor color
   final Color? cursorColor;
+  final TextStyle? titleTextStyle;
+  final String searchHintText;
+  final IconThemeData? searchBackIconTheme;
+  final Color? searchCursorColor;
+  final TextStyle? searchHintStyle;
+  final TextStyle searchTextStyle;
+  final Duration debounceDuration;
+  final TextStyle suggestionTextStyle;
+  final Color? suggestionBackgroundColor;
 
   const EasySearchBar({
     Key? key,
     required this.title,
     required this.onSearch,
     this.actions = const [],
+    this.searchHintStyle,
+    this.searchTextStyle = const TextStyle(),
     this.suggestions,
+    this.asyncSuggestions,
+    this.searchCursorColor,
+    this.searchHintText = '',
+    this.suggestionsElevation = 5,
+    this.backgroundColor,
     this.foregroundColor,
+    this.elevation,
     this.toolbarHeight = 56,
     this.centerTitle = false,
     this.cursorColor,
     this.isFloating = false,
+    this.titleTextStyle,
+    this.iconTheme,
+    this.searchBackIconTheme,
+    this.suggestionTextStyle = const TextStyle(),
+    this.suggestionBackgroundColor,
     this.animationDuration = const Duration(milliseconds: 450),
+    this.debounceDuration = const Duration(milliseconds: 400)
   }) : super(key: key);
 
   @override
@@ -77,12 +106,13 @@ class _EasySearchBarState extends State<EasySearchBar> with TickerProviderStateM
   List<String> _suggestions = [];
   Timer? _debounce;
   String _previousAsyncSearchText = '';
+  final FocusNode _focusNode = FocusNode();
 
   late AnimationController _controller;
   late Animation _containerSizeAnimation;
   late Animation _containerBorderRadiusAnimation;
   late Animation _textfieldOpacityAnimation;
-  late TextEditingController _searchController;
+  final TextEditingController _searchController = TextEditingController();
 
   @override
   void initState() {
@@ -109,24 +139,24 @@ class _EasySearchBarState extends State<EasySearchBar> with TickerProviderStateM
         curve: const Interval(0.45, 1, curve: Curves.easeIn),
       ),
     );
-   // _controller.forward();
-    _searchController = TextEditingController();
-    _searchController.addListener(() {
+    _searchController.addListener(() async {
       widget.onSearch(_searchController.text);
+      if (widget.suggestions != null) {
+        updateSyncSuggestions(_searchController.text);
+      }
+      else if (widget.asyncSuggestions != null) {
+         updateAsyncSuggestions(_searchController.text);
+      }
     });
-    // TODO: set async suggestions
     // TODO: detect key enter press to dismiss suggestions
     // TODO: set suggestions list animation
-
-    // TODO: add suppirt for back button in appbar
-    Future.delayed(Duration.zero, () async {  _suggestions = await widget.suggestions!; });
   }
 
   void openOverlay() {
-    if (_overlayEntry == null && widget.suggestions != null) {
+    if (_overlayEntry == null && (widget.suggestions != null || widget.asyncSuggestions != null)) {
       RenderBox renderBox = context.findRenderObject() as RenderBox;
-      var size = renderBox.size;
-      var offset = renderBox.localToGlobal(Offset.zero);
+      Size size = renderBox.size;
+      Offset offset = renderBox.localToGlobal(Offset.zero);
 
       _overlayEntry ??= OverlayEntry(
         builder: (context) => Positioned(
@@ -142,35 +172,29 @@ class _EasySearchBarState extends State<EasySearchBar> with TickerProviderStateM
                 maxHeight: 150
               ),
               margin: const EdgeInsets.all(5),
-              child: Material(
-                elevation: 5,
-                color: Colors.white,
-                borderRadius: BorderRadius.circular(5),
-                child: ListView.builder(
-                  shrinkWrap: true,
-                  padding: EdgeInsets.zero,
-                  itemCount: _suggestions.length,
-                  itemBuilder: (context, index) {
-                    return InkWell(
-                      child: Container(
-                        width: double.infinity,
-                        padding: const EdgeInsets.all(10),
-                        child: Text(
-                          _suggestions[index],
-                          // thisstyle: suggestionTextStyle
-                        )
-                      ),
-                      onTap: () => _searchController.text = _suggestions[index]
-                    );
-                  },
-                ),
-              ),
+              child: FilterableList(
+                loading: _isLoading,
+                items: _suggestions,
+                elevation: widget.suggestionsElevation,
+                suggestionTextStyle: widget.suggestionTextStyle,
+                suggestionBackgroundColor: widget.suggestionBackgroundColor,
+                onItemTapped: (value) {
+                  _searchController.value = TextEditingValue(
+                    text: value,
+                    selection: TextSelection.collapsed(
+                      offset: value.length
+                    )
+                  );
+                  widget.onSearch(value);
+                  closeOverlay();
+                }
+              )
             )
           )
         )
       );
     }
-    if (!_hasOpenedOverlay && widget.suggestions != null) {
+    if (!_hasOpenedOverlay && (widget.suggestions != null || widget.asyncSuggestions != null)) {
       Overlay.of(context)!.insert(_overlayEntry!);
       setState(() => _hasOpenedOverlay = true );
     }
@@ -183,15 +207,30 @@ class _EasySearchBarState extends State<EasySearchBar> with TickerProviderStateM
     }
   }
 
-  Future<void> updateSuggestions(String input) async {
+  void updateSyncSuggestions(String input) {
+    openOverlay();
+    _suggestions = widget.suggestions!.where((element) {
+      return element.toLowerCase().contains(input.toLowerCase());
+    }).toList();
     rebuildOverlay();
-    if (widget.suggestions != null) {
-      _suggestions = await widget.suggestions!;
-      _suggestions = _suggestions.where((element) {
-        return element.toLowerCase().contains(input.toLowerCase());
-      }).toList();
-      rebuildOverlay();
+  }
+
+  Future<void> updateAsyncSuggestions(String input) async {
+    openOverlay();
+    if (_debounce != null && _debounce!.isActive) {
+      _debounce!.cancel();
     }
+    setState(() => _isLoading = true);
+    _debounce = Timer(widget.debounceDuration, () async {
+      if (_previousAsyncSearchText != input || _previousAsyncSearchText.isEmpty || input.isEmpty) {
+        _suggestions = await widget.asyncSuggestions!(input);
+        setState(() {
+          _isLoading = false;
+          _previousAsyncSearchText = input;
+        });
+        rebuildOverlay();
+      }
+    });
   }
 
   void rebuildOverlay() {
@@ -205,13 +244,33 @@ class _EasySearchBarState extends State<EasySearchBar> with TickerProviderStateM
     final ThemeData theme = Theme.of(context);
     final AppBarTheme appBarTheme = AppBarTheme.of(context);
     final ScaffoldState? scaffold = Scaffold.maybeOf(context);
+    final ModalRoute<dynamic>? parentRoute = ModalRoute.of(context);
 
-    Color? foregroundColor = widget.foregroundColor ?? appBarTheme.foregroundColor ?? theme.primaryColor;
-    IconThemeData overallIconTheme = appBarTheme.iconTheme
-        ?? theme.iconTheme.copyWith(color: foregroundColor);
+    final bool canPop = parentRoute?.canPop ?? false;
 
-    TextStyle? titleTextStyle = appBarTheme.titleTextStyle
-        ?? theme.textTheme.headline6;
+    Color? backgroundColor = widget.backgroundColor ?? appBarTheme.backgroundColor ?? theme.primaryColor;
+
+    Color? foregroundColor = widget.foregroundColor ?? appBarTheme.foregroundColor;
+
+    IconThemeData iconTheme = widget.iconTheme ?? appBarTheme.iconTheme ?? theme.iconTheme.copyWith(color: foregroundColor);
+
+    TextStyle? titleTextStyle = widget.titleTextStyle ?? appBarTheme.titleTextStyle ?? theme.textTheme.headline6!.copyWith(color: foregroundColor);
+
+    double? elevation = widget.elevation ?? appBarTheme.elevation ?? 5;
+
+    Color searchColor = theme.brightness == Brightness.light ? Colors.white : Colors.black;
+
+    Color cursorColor = widget.searchCursorColor ?? theme.primaryColor;
+
+    IconThemeData searchIconThemeData = widget.searchBackIconTheme ?? IconThemeData(
+      size: 24,
+      color: Theme.of(context).primaryColor
+    );
+
+    TextStyle searchHintStyle = widget.searchHintStyle ?? theme.inputDecorationTheme.hintStyle ?? const TextStyle(
+      color: Colors.grey,
+      fontStyle: FontStyle.italic
+    );
 
     return CompositedTransformTarget(
       link: _layerLink,
@@ -222,8 +281,8 @@ class _EasySearchBarState extends State<EasySearchBar> with TickerProviderStateM
             return Container(
               margin: EdgeInsets.all(widget.isFloating ? 5 : 0),
               child: Material(
-                elevation: 5,
-                color: foregroundColor,
+                elevation: elevation,
+                color: backgroundColor,
                 child: Stack(
                   children: [
                     Container(
@@ -242,18 +301,31 @@ class _EasySearchBarState extends State<EasySearchBar> with TickerProviderStateM
                         children: [
                           Visibility(
                             visible: scaffold?.hasDrawer ?? false,
-                            child: IconButton(
-                              icon: const Icon(Icons.menu),
-                              iconSize: overallIconTheme.size ?? 24,
-                              onPressed: () => scaffold!.openDrawer(),
-                              tooltip: MaterialLocalizations.of(context).openAppDrawerTooltip,
+                            child: IconTheme(
+                              data: iconTheme,
+                              child: IconButton(
+                                icon: const Icon(Icons.menu),
+                                iconSize: iconTheme.size ?? 24,
+                                onPressed: () => scaffold!.openDrawer(),
+                                tooltip: MaterialLocalizations.of(context).openAppDrawerTooltip,
+                              ),
+                            ),
+                            replacement: Visibility(
+                              visible: canPop,
+                              child: IconButton(
+                                icon: const Icon(Icons.arrow_back_outlined),
+                                iconSize: iconTheme.size ?? 24,
+                                onPressed: () => Navigator.pop(context),
+                                tooltip: MaterialLocalizations.of(context).backButtonTooltip
+                              ),
+                              replacement: const SizedBox()
                             )
                           ),
                           Expanded(
                             child: Container(
                               margin: const EdgeInsets.only(left: 20),
                               child: DefaultTextStyle(
-                                style: titleTextStyle!,
+                                style: titleTextStyle,
                                 softWrap: false,
                                 overflow: TextOverflow.ellipsis,
                                 child: widget.title,
@@ -262,18 +334,24 @@ class _EasySearchBarState extends State<EasySearchBar> with TickerProviderStateM
                           ),
                           ...List.generate(widget.actions.length + 1, (index) {
                             if (widget.actions.length == index) {
-                              return IconButton(
-                                icon: const Icon(Icons.search),
-                                iconSize: overallIconTheme.size ?? 24,
-                                onPressed: () {
-                                  _controller.forward();
-                                  openOverlay();
-                                },
-                                tooltip: MaterialLocalizations.of(context).openAppDrawerTooltip,
+                              return IconTheme(
+                                data: iconTheme,
+                                child: IconButton(
+                                  icon: const Icon(Icons.search),
+                                  iconSize: iconTheme.size ?? 24,
+                                  onPressed: () {
+                                    _controller.forward();
+                                    _focusNode.requestFocus();
+                                  },
+                                  tooltip: MaterialLocalizations.of(context).searchFieldLabel,
+                                ),
                               );
                             }
                 
-                            return widget.actions[index];
+                            return IconTheme(
+                              data: iconTheme,
+                              child: widget.actions[index]
+                            );
                           })
                         ]
                       )
@@ -289,53 +367,61 @@ class _EasySearchBarState extends State<EasySearchBar> with TickerProviderStateM
                             width: _containerSizeAnimation.value * constraints.maxWidth,
                             decoration: BoxDecoration(
                               borderRadius: BorderRadius.only(
-                                bottomLeft: Radius.circular(_containerBorderRadiusAnimation.value * 50),
-                                topLeft: Radius.circular(_containerBorderRadiusAnimation.value * 50),
+                                bottomLeft: Radius.circular(_containerBorderRadiusAnimation.value * 30),
+                                topLeft: Radius.circular(_containerBorderRadiusAnimation.value * 30),
                               ),
-                              color: Colors.white
+                              color: searchColor
                             ),
                             child: Opacity(
                               opacity: _textfieldOpacityAnimation.value,
                               child: TextField(
                                 controller: _searchController,
-                                autofocus: true,
+                                cursorColor: cursorColor,
+                                focusNode: _focusNode,
                                 textAlignVertical: TextAlignVertical.center,
+                                style: widget.searchTextStyle,
                                 decoration: InputDecoration(
                                   contentPadding: const EdgeInsets.only(
                                     left: 20,
-                                    right: 10,
+                                    right: 10
                                   ),
+                                  hintText: widget.searchHintText,
+                                  hintMaxLines: 1,
+                                  hintStyle: searchHintStyle,
                                   border: InputBorder.none,
-                                  suffixIcon: IconButton(
-                                    color: Colors.red,
-                                    icon: const Icon(
-                                      Icons.close
-                                    ),
-                                    onPressed: () {
-                                      closeOverlay();
-                                      _controller.reverse();
-                                      _searchController.clear();
-                                    }
+                                  prefixIcon: IconTheme(
+                                    data: searchIconThemeData,
+                                    child: IconButton(
+                                      icon: const Icon(
+                                        Icons.arrow_back_outlined
+                                      ),
+                                      onPressed: () {
+                                        _controller.reverse();
+                                        _searchController.clear();
+                                        closeOverlay();
+                                      }
+                                    )
                                   )
                                 )
-                              ),
+                              )
                             )
                           );
-                        },
-                      ),
+                        }
+                      )
                     )
                   ]
                 )
-              ),
+              )
             );
           }
         )
-      ),
+      )
     );
   }
 
   @override
   void dispose() {
+    _focusNode.dispose();
     _searchController.dispose();
     super.dispose();
   }
